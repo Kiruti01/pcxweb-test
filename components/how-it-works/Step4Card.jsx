2d16a9749e50eee8133adb3cc027bd113261f4dc
Michael Kiruti <mikekirutic@gmail.com>
2026-05-06 10:46:48 +0300

    Change font weight from extrabold to semibold

--- components/how-it-works/Step4Card.jsx
+++ components/how-it-works/Step4Card.jsx
@@ -188,7 +188,7 @@ const Step4Card = () => {
           Step 4
         </span>
         <h3
-          className="font-extrabold mb-2 leading-snug"
+          className="font-semibold mb-2 leading-snug"
           style={{
             fontSize: "16px",
             letterSpacing: "-0.3px",
