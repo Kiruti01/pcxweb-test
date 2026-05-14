9810d45f32bc95c0957cd1f5a6c7c0af0fb8fb55
Michael Kiruti <mikekirutic@gmail.com>
2026-05-14 10:21:02 +0300

    Import User model in posts route

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { connectDB } from "@/lib/mongodb";
 import Post from "@/models/Post";
+import "@/models/User";
 
 export async function GET(req) {
   try {
